5a1d1fc6b6e54ed0475ad4fab237074893a2ef75
Subhendu Hazra <subhendu965.hazra@gmail.com>
2022-10-25 02:08:55 +0530

    web component implemented

--- script.js
+++ script.js
@@ -131,17 +131,6 @@ mns.showFloodFill=(row,col)=>{
         
 }
 
-let boolMenu=false;
-function showMenu(){
-
-    if(!boolMenu){
-        document.querySelector("header").classList.add("open_menu");
-    }else{
-        document.querySelector("header").classList.remove("open_menu");
-    }
-    boolMenu=!boolMenu;    
-    // alert(boolMenu);
-}
 
 function restart(){
     mns.restartGame();
